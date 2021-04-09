7ccf6af68b7f2809b06b4df6346184732458127a
AlexisFioreCode <alexis.fiorentino.pro@gmail.com>
2021-04-09 13:16:30 +0200

    Blog page finished with html done by javascript

--- js/index.js
+++ js/index.js
@@ -1,20 +1,18 @@
+let layer = document.getElementById("layer");
 function loadLayer() {
     let httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if (httpRequest.status === 200) {      
-                let data = httpRequest.responseText;
-                console.log(data)
+            if (httpRequest.status === 200) {   
+                layer.innerHTML = this.responseText;              
             } 
-            else {
-                ajaxContent.innerText = "Nous n'avons pas réussi à récupérer le contenu";
-            }
         } 
-        else {
-            ajaxContent.innerText = "Requête en cours";
-        }
     };
 
     httpRequest.open('GET', 'layer.txt', true);
     httpRequest.send();
+}
+function removeLayer() {
+    if(layer !== null)
+    layer.remove()   
 }
